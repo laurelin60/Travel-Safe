ac90b2401ae2b5ed6ac9880065c398f5b55b6526
Kevin Wu <kevinwu098@gmail.com>
2024-04-07 02:20:34 -0700

    feat: create server implementation in js

---
+++ socket/dummy_server.js
@@ -0,0 +1,46 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+const clients = {};
+
+wss.on("connection", (ws, req) => {
+    console.log("Connection established with",req.socket.remoteAddress, ws._socket.address().port)
+    const id = generateId();
+    clients[id] = ws;
+
+    ws.send(JSON.stringify({ type: "id", id }));
+
+    ws.on("message", (message) => {
+        message = JSON.parse(message);
+        let senderId = "";
+
+        if (message["id"]) {
+            senderId = message["id"];
+        }
+
+        sendToOthers(senderId, JSON.stringify({ type: message["type"] }));
+    });
+
+    ws.on("close", () => {
+        delete clients[id];
+    });
+});
+
+function generateId() {
+    while (true) {
+        const id = Math.random().toString(36).substring(7);
+        if (!clients[id]) {
+            return id;
+        }
+    }
+}
+
+function sendToOthers(senderId, message) {
+    for (const id in clients) {
+        if (id !== senderId) {
+            clients[id].send(message);
+        }
+    }
+}
+
+console.log("WebSocket server running on port 8080");
